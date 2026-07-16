3579ef08e3076e23a63035fa9a011c3f1f5dfebe
Juan Tenreyro <tenreyrojuan1@gmail.com>
2026-07-16 14:17:05 -0300

    feature(domain): agregar dias de la semana en español

---
+++ Dsw2026Tpi.Domain/Entities/DayOfWeekSpanish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Entities;
+
+public enum DayOfWeekEs
+{
+    Domingo = 0,
+    Lunes,
+    Martes,
+    Miercoles,
+    Jueves,
+    Viernes,
+    Sabado
+}
+
