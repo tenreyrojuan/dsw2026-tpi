c06d918a932bc326b91476114e8f2326b88ea0fc
Juan Tenreyro <tenreyrojuan1@gmail.com>
2026-07-19 00:05:36 -0300

    feature(crosscutting): agregar metodos de validacion y mensaje de codigo de error 'BusinessRuleException'

--- Dsw2026Tpi.CrossCutting/Helpers/ValidationsExtensions.cs
+++ Dsw2026Tpi.CrossCutting/Helpers/ValidationsExtensions.cs
@@ -10,4 +10,17 @@ public static class ValidationsExtensions
         return !string.IsNullOrWhiteSpace(email) &&
             Regex.IsMatch(email, EmailPattern);
     }
+    public static bool IsNameValid(this string? name)
+    {
+        return !string.IsNullOrWhiteSpace(name) &&
+            (name.Length >= 3 && name.Length <= 100);
+    }
+    public static bool IsMonthValid(this DateOnly month)
+    {
+        return month.Month >= 1 && month.Month <= 12;
+    }
+    public static bool IsYearValid(this DateOnly year)
+    {
+        return year.Year.Equals(DateTime.Now.Year);
+    }
 }
