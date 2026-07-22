4f6337cd651171ec74ccb598a98aafff771d3e7f
Joaquin V. <joacovaldecantos@gmail.com>
2026-07-22 16:36:57 -0300

    feat(application): agregar DTO, interfaces y servicio para especialidades.

---
+++ Dsw2026Tpi.Application/Dtos/SpecialityModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Dsw2026Tpi.Application.Dtos;
+
+public record SpecialityModel
+{
+    public record Request(string Name, string Description);
+    public record Response(Guid Id, string Name, string Description);
+}
