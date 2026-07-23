b36d290a47619670526f5f9c12d284d9f22daf2a
Juan Tenreyro <tenreyrojuan1@gmail.com>
2026-07-23 15:10:19 -0300

    refactor(application): cambios en el DoctorModel y en el servicio

--- Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
+++ Dsw2026Tpi.Application/Interfaces/IDoctorService.cs
@@ -6,9 +6,9 @@ namespace Dsw2026Tpi.Application.Interfaces;
 public interface IDoctorService
 {
     Task<Pagination<DoctorModel.Response>> GetAll(int pageSize, int pageIndex, string? name = null);
-    Task<IEnumerable<DoctorModel.AvailabilityDto>> GetDoctorAvailabilities(Guid id,DateOnly date);
-    Task AddDoctor(string name, string licenseNumber, Guid specialityId);
-    Task UpdateDoctor(Guid id, string name, string licenseNumber, Guid specialityId);
+    Task<IEnumerable<DoctorAvailabilityModel.Response>> GetDoctorAvailabilities(DoctorAvailabilityModel.Request request);
+    Task AddDoctor(DoctorModel.Request request);
+    Task UpdateDoctor(DoctorUpdateModel.Request request);
     Task DeleteDoctor(Guid id);
 
 }
