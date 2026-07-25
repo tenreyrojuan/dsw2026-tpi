43e05aa2d7a225d46c6e131e1b46d0918503b943
Alarcón Lucas <lucasalarcontrevisani@alu.frt.utn.edu.ar>
2026-07-24 22:09:33 -0300

    feature (api): agregar endpoint de la disponibilidad

--- Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
+++ Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
@@ -13,6 +13,7 @@ public static class DependencyInjectionConfigurationExtensions
         services.AddScoped<IPersistence, PersistenceEf>();
         services.AddScoped<IDoctorService, DoctorService>();
         services.AddScoped<ISpecialityService, SpecialityService>();
+        services.AddScoped<IAvailabilityService, AvailabilityService>();
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<ISignInService, SignInService>();
         services.AddSingleton<JwtService>();
