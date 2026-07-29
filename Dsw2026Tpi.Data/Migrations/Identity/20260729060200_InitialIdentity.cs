c35405c777febc501ae47e51eaa952e9920973de
Joaquin V. <joacovaldecantos@gmail.com>
2026-07-29 03:06:37 -0300

    refactor(data): añadir migraciones luego de cambio en las configurations.

--- Dsw2026Tpi.Data/Migrations/Identity/20260729060200_InitialIdentity.cs
+++ Dsw2026Tpi.Data/Migrations/Identity/20260729060200_InitialIdentity.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Dsw2026Tpi.Data.Migrations.Identity
 {
     /// <inheritdoc />
-    public partial class Initial_Model : Migration
+    public partial class InitialIdentity : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
