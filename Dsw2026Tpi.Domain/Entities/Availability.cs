91db9ac466b87ff6aa4cd273dc56afc446e393d4
Joaquin V. <joacovaldecantos@gmail.com>
2026-07-29 00:54:21 -0300

    refactor(domain): eliminar DaysOfWeekEs, y modificar en availability.

--- Dsw2026Tpi.Domain/Entities/Availability.cs
+++ Dsw2026Tpi.Domain/Entities/Availability.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
-
-namespace Dsw2026Tpi.Domain.Entities;
+﻿namespace Dsw2026Tpi.Domain.Entities;
 //Disponibilidad
 public class Availability : EntityBase
 {
-    /// <summary>
-    /// se cambiaron los modificadores de acceso de algunas propiedades
-    /// para poder actualizar las disponibilidades
-    /// </summary>
-    public int Month { get; init; } 
+    public int Month { get; init; }
     public int Year { get; init; }
-    public DaysOfWeekEs WeekDay { get; private set; }
+
+    public DayOfWeek WeekDay { get; private set; }
     public TimeOnly StartingHour { get; private set; }
     public TimeOnly EndingHour { get; private set; }
 
     public Guid DoctorId { get; private set; }
-    
     public Doctor Doctor { get; private set; }
 
     public ICollection<TimeSlot> TimeSlots { get; private set; } = [];
@@ -30,7 +21,7 @@ public class Availability : EntityBase
     }
 #pragma warning restore CS8618
     #endregion
-    public Availability(int month, int year, DaysOfWeekEs weekDay, TimeOnly startingHour,
+    public Availability(int month, int year, DayOfWeek weekDay, TimeOnly startingHour,
         TimeOnly endingHour, Doctor doctor, Guid? id = null) : base(id)
     {
         Month = month;
@@ -46,12 +37,10 @@ public class Availability : EntityBase
         TimeSlots.Add(timeSlot);
     }
 
-    //metodo nuevo actualizar disponibilidad
-    public void UpdateAvailability(DaysOfWeekEs weekDay, TimeOnly startingHour, TimeOnly endingHour, Guid doctorId)
+    public void UpdateAvailability(DayOfWeek weekDay, TimeOnly startingHour, TimeOnly endingHour, Guid doctorId)
     {
         WeekDay = weekDay;
         StartingHour = startingHour;
         EndingHour = endingHour;
-        DoctorId = doctorId;
     }
 }
