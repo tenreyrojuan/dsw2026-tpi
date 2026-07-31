5b1a6d6a4025bc8ef7c9990343c0a3dd071ce5d0
Alarcón Lucas <lucasalarcontrevisani@alu.frt.utn.edu.ar>
2026-07-30 23:52:25 -0300

    refactor(Data): Refactorizar AplicationUser

--- Dsw2026Tpi.Data/Identity/ApplicationUser.cs
+++ Dsw2026Tpi.Data/Identity/ApplicationUser.cs
@@ -4,6 +4,7 @@ namespace Dsw2026Tpi.Data.Identity;
 
 public class ApplicationUser: IdentityUser
 {
+    public long? Dni { get; set; }
     public bool Deleted { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
